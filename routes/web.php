ba24e8084a073864f23bafd0d692804779adebe7
zhawenbao <863973855@qq.com>
2018-12-07 16:54:08 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@
 Route::prefix('admin')->group(function () { //后台路由
     Route::get('index', "Admin\Index@index"); //首页
     Route::get('welcome', "Admin\Index@welcome"); //欢迎页面
+    Route::get('article_list', "Admin\Article@list"); //资讯管理
+    Route::get('article_add', "Admin\Article@add"); //添加资讯管理
+    Route::get('picture_add', "Admin\Picture@add"); //添加图片
+    Route::get('picture_list', "Admin\Picture@list"); //图片列表
+    Route::get('picture_show', "Admin\Picture@show"); //图片编辑
 
 });
 //Route::get('admin.index', 'Admin/Index@index');
