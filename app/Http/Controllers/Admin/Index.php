0b51c4d357afa6e5bfe32d343ad5405da9ea3fa6
zhawenbao <863973855@qq.com>
2018-12-17 09:54:42 +0800

    no message

--- app/Http/Controllers/Admin/Index.php
+++ app/Http/Controllers/Admin/Index.php
@@ -8,13 +8,32 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 
 class Index extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 'one';
+        $out_trade_no =date('YmdHis111');          //公共方法生成唯一订单号
+        $subject = 'test';                           //数据仅供测试，下同
+        $total_amount = 0.01;
+        $body = 'test test!';
+        $timeout_express="1m";
+
+        $payRequestBuilder = new AlipayTradeWapPayContentBuilder();
+        $payRequestBuilder->setBody($body);
+
+        $payRequestBuilder->setSubject($subject);
+        $payRequestBuilder->setOutTradeNo($out_trade_no);
+        $payRequestBuilder->setTotalAmount($total_amount);
+
+        $payRequestBuilder->setTimeExpress($timeout_express);
+
+
+        $payResponse = new AlipayTradeService();
+
+        $result=$payResponse->wapPay($payRequestBuilder,config('alipay.return_url'),config('alipay.notify_url'));
 //        return view("admin.index.index");
     }
 
